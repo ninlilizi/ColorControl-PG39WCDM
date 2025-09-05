98bc2d4a35d170d0ad12bb7820c237939baad61e
Abigail of Newbury <nin@nkli.net>
2025-09-05 15:31:21 +0100

    Update some variable default values.

--- MHC2Gen/GenerateProfileCommand.cs
+++ MHC2Gen/GenerateProfileCommand.cs
@@ -7,18 +7,18 @@ public class GenerateProfileCommand
     public string Description { get; set; } = "";
     public bool HasExtraInfo { get; set; }
     public bool IsHDRProfile { get; set; }
-    public RgbPrimaries DevicePrimaries { get; set; } = new RgbPrimaries(RgbPrimaries.sRGB);
+    public RgbPrimaries DevicePrimaries { get; set; } = new RgbPrimaries(RgbPrimaries.P3D65);
     public SDRTransferFunction SDRTransferFunction { get; set; }
     public ColorGamut ColorGamut { get; set; }
     public double MinCLL { get; set; }
     public double MaxCLL { get; set; }
-    public double WhiteLuminance { get; set; } = 2000;
+    public double WhiteLuminance { get; set; } = 1000;
     public double BlackLuminance { get; set; }
     public double SDRMinBrightness { get; set; }
     public double SDRMaxBrightness { get; set; } = 100;
     public double SDRBrightnessBoost { get; set; }
     public double ShadowDetailBoost { get; set; }
-    public double Gamma { get; set; } = 2.2;
+    public double Gamma { get; set; } = 2.4;
 
     public double ToneMappingFromLuminance { get; set; }
     public double ToneMappingToLuminance { get; set; }
@@ -30,5 +30,5 @@ public class GenerateProfileCommand
     /// WOLED desaturation compensation strength (0-100). 
     /// Widens gamut at low luminance levels to compensate for WOLED display desaturation.
     /// </summary>
-    public double WOLEDDesaturationCompensation { get; set; } = 90;
+    public double WOLEDDesaturationCompensation { get; set; } = 95;
 }
